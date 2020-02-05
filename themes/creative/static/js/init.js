7458263c48e0b02e386eda00778ab6b49ebbfba3
Jerry <8124783+nianiaJR@users.noreply.github.com>
2020-02-05 19:54:05 +0800

    fix: theme (#17)

--- themes/creative/static/js/init.js
+++ themes/creative/static/js/init.js
@@ -1,8 +1,8 @@
 var language = (navigator.browserLanguage || navigator.language).toLowerCase();
 if (window.location.pathname == '/') {
   if (language.indexOf('zh') > -1) {
-    window.location.href = window.location + '/cn/'
+    window.location.href =  '/cn/'
   } else {
-    window.location.href = window.location + '/en/'
+    window.location.href = '/en/'
   }
 }
